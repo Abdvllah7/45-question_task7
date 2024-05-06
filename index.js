25e1631273c1f0825c41ca50701bbfccb3eabb42
Abdvllah7 <czczcsxvdg@gmail.com>
2024-05-06 05:42:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+var personName = "\n\t Abdullah \t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ index.ts
@@ -0,0 +1,4 @@
+var personName: string = "\n\t Abdullah \t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
